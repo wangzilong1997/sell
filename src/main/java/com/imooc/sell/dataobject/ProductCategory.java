1eb3bd860a919756781120673fb930030a9f0d83
wangzilong1997 <1321787581@qq.com>
2020-02-28 15:11:29 +0800

    11-4卖家类目功能开发

--- src/main/java/com/imooc/sell/dataobject/ProductCategory.java
+++ src/main/java/com/imooc/sell/dataobject/ProductCategory.java
@@ -27,9 +27,28 @@ public class ProductCategory {
     /**类目编号**/
     private Integer categoryType;
 
-    public ProductCategory() {
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Date getCreateTime() {
+        return createTime;
     }
 
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public ProductCategory() {
+    }
 
 
     public ProductCategory(Integer categoryType) {
